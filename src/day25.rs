1d2b3bedbbb6dd5ffa5d307d6fe09bcc5b20e9a5
Elviss Kustans <n3o59hf@gmail.com>
2024-12-25 07:39:24 +0100

    Optimize day 25 solution

--- src/day25.rs
+++ src/day25.rs
@@ -12,63 +12,54 @@ pub fn part2(_: &str) -> String {
 
 type Input = (Vec<u32>, Vec<u32>);
 
+const LOCKS: [u32; 6] = [0b00000, 0b00001, 0b00011, 0b00111, 0b01111, 0b11111];
+
+const KEYS: [u32; 6] = [0b00000, 0b10000, 0b11000, 0b11100, 0b11110, 0b11111];
+
 #[aoc_generator(day25)]
 fn parse(input: &str) -> Input {
-    let mut locks: Vec<u32> = Vec::with_capacity(250);
-    let mut keys: Vec<u32> = Vec::with_capacity(250);
-    let mut number: [u8; 5] = [0, 0, 0, 0, 0];
-    for c in input.split("\n\n") {
+    let mut locks: Vec<u32> = Vec::new();
+    let mut keys: Vec<u32> = Vec::new();
+    let mut number: [usize; 5] = [0, 0, 0, 0, 0];
+
+    let bytes = input.as_bytes();
+    let stop_point = bytes.len() - 8;
+    let mut cursor = 0usize;
+    while cursor < stop_point {
         number.fill(0);
-        for line in c.lines().skip(1).take(5) {
-            for (i, c) in line.chars().enumerate() {
-                if c == '#' {
-                    number[4 - i] += 1;
+        let is_key = bytes[cursor] == b'.';
+        cursor += 6; // skip first line
+        for _ in 0..5 {
+            for i in 0..5 {
+                if bytes[cursor + i] == b'#' {
+                    number[i] += 1;
                 }
             }
+            cursor += 6;
         }
-        if &c[0..1] == "." {
-            //key
-            let mut pins = 0u32;
-            for i in number {
-                for _ in 0..i {
-                    pins <<= 1;
-                    pins += 1;
-                }
-                for _ in 0..(5 - i) {
-                    pins <<= 1;
-                }
-            }
-            keys.push(pins);
-        } else {
-            //lock
-            let mut pins = 0u32;
-            for i in number {
-                for _ in 0..(5 - i) {
-                    pins <<= 1;
-                }
-                for _ in 0..i {
-                    pins <<= 1;
-                    pins += 1;
-                }
-            }
-            locks.push(pins);
+        cursor += 7; // skip last line and empty line
+        let map = if is_key { KEYS } else { LOCKS };
+        let mut pins = 0u32;
+        for i in number {
+            pins <<= 5;
+            pins |= map[i] << (5 * i);
         }
+
+        if is_key { &mut keys } else { &mut locks }.push(pins);
     }
     (keys, locks)
 }
 
 #[aoc(day25, part1)]
 fn part1_solution(input: &Input) -> u32 {
-    let (keys, locks) = input.clone();
     let mut counter = 0u32;
-    for lock in locks {
-        for &key in &keys {
-            if key & lock == 0 {
+    for &a in &input.0 {
+        for &b in &input.1 {
+            if a & b == 0 {
                 counter += 1;
             }
         }
     }
-
     counter
 }
 
